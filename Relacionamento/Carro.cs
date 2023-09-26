d76225e27e404aaa054ff9e65dd6e084fd15e526
Agata Domingues <124289385+htadmg@users.noreply.github.com>
2023-09-26 09:08:41 -0300

    Add project files.

---
+++ Relacionamento/Carro.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Relacionamento
+{
+    public class Carro 
+    {
+        public int Id { get; set; }
+        public string ModeloId { get; set; }
+        public string Nome { get; set; }
+        public int Ano { get; set;  }
+    }
+}
---
+++ Relacionamento/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Relacionamento
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+}
